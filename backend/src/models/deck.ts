cebd87f632806f98ede7665f89fdb2a1fd1b7333
caleb <calebcooper86@gmail.com>
2023-04-29 21:48:43 +0100

    added the user and decks models

---
+++ backend/src/models/deck.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const deckSchema = new mongoose.Schema({
+   topic: {
+        type: String,
+        required: true
+   },
+   likes: {
+        type: Number,
+        default: 0
+   },
+   cards: [{
+     front: {
+          type: String,
+          required: true
+     },
+     back: {
+          type: String,
+          required: true
+     },
+     required: true
+   }],
+   user: {
+     type: mongoose.Schema.Types.ObjectId,
+     ref: 'User'
+   }
+})
+
+deckSchema.set('toJSON', {
+     transform: (document, returnedObject) => {
+          returnedObject.id = returnedObject._id.toString();
+          delete returnedObject._id;
+          delete returnedObject.__v;
+     } 
+});
+
+export const deck = mongoose.model('Deck', deckSchema);
---
+++ backend/src/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 2,
+    },
+    userName:{
+        type: String,
+        required: true,
+    },
+    hashPassword: {
+        type: String,
+        required: true,
+        minLength: 5,
+    },
+    cards: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Card'
+    }]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString(),
+        delete returnedObject._id,
+        delete returnedObject.__v,
+        delete returnedObject.hashPassword
+    }
+});
+
+export const User = mongoose.model('User', userSchema);
+
