ea4beccfafa93be38c2ceb9d2f9213d21a3186e0
caleb <calebcooper86@gmail.com>
2023-05-19 23:30:40 +0100

    small changes to display

--- frontend/src/components/DeckDisplay/DeckDisplay.tsx
+++ frontend/src/components/DeckDisplay/DeckDisplay.tsx
@@ -14,12 +14,13 @@ export default function DeckDisplay({deck, isUserDecks}: Props) {
     <div className='deck-grid-item-template group'>
         <p className='text-base line-clamp-2'>Topic: {deck.topic}</p>
         <p className='text-xs line-clamp-2'>Number of cards: {deck.cards.length}</p>
+        <p className='text-xs'>Tags: {deck.tags.map((tag => <span>{tag} </span>))} </p>
         <p className='text-xs truncate'>Likes: {deck.likes}</p>
         <div className='absolute flex flex-col justify-center items-center scale-0 w-full h-full 
         top-0 left-0 origin-center bg-purple-900 opacity-90 group-hover:scale-100 
         transition-all duration-200 font-black '>
             <Link to={`/viewDeck/${deck.id}`}><button className='hover:underline hover:text-black'>View</button></Link>
-            <button className='hover:underline hover:text-black'>Edit</button>
+            {isUserDecks && <button className='hover:underline hover:text-black'>Edit</button>}
             {isUserDecks && <DeleteDeckBtn id={deck.id} />}
         </div>
     </div>
